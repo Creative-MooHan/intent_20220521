b10216406db23ede886bbc45f14b55bc12a9cfa5
이무한 <klaw96@naver.com>
2022-05-21 14:43:54 +0900

    String을 다른 화면으로 넘겨주기 - UI 그리기

--- app/src/main/java/com/nepplus/intent_20220521/OtherActivity.kt
+++ app/src/main/java/com/nepplus/intent_20220521/OtherActivity.kt
@@ -1,11 +1,29 @@
 package com.nepplus.intent_20220521
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnMoveBack.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+
+            finish()
+
+
+
+
+        }
+
+
+
     }
 }
