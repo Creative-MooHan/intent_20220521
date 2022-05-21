d78da89aa25a44bfbea301ba189abfe8b9f2baf3
이무한 <klaw96@naver.com>
2022-05-21 14:58:49 +0900

    문구 첨부 + 이동한 화면에서 받아서 사용

--- app/src/main/java/com/nepplus/intent_20220521/ViewMessageActivity.kt
+++ app/src/main/java/com/nepplus/intent_20220521/ViewMessageActivity.kt
@@ -2,10 +2,22 @@ package com.nepplus.intent_20220521
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+//        화면에 들어오자마자>보내준 메세지를 추출 > 텍스트뷰에 반영
+//        이벤트 처리 X. 바로 실행
+
+        val receivedMessage = intent.getStringExtra("msg")
+
+        txtMessage.text = receivedMessage
+
+
+
+
     }
 }
