9724d9762cdfc4ab60b64e8c086e2144f43bd3cd
이무한 <klaw96@naver.com>
2022-05-21 14:34:30 +0900

    Intent를 이용 Other 액티비티로 이동

--- app/src/main/java/com/nepplus/intent_20220521/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_20220521/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nepplus.intent_20220521
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,6 +11,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnMove.setOnClickListener {
+
+            다른 화면으로 이동하기
+
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+
 
 
 
